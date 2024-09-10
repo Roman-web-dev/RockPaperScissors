91937f38ab43c392f246e8db5b0f4927a5d24706
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-09-10 15:09:14 -0400

    Add function for human input
    Add tests for validation of human input

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("Working")
 
 // A function that returns ramdomly rock paper or scissors
     
-    // declare function
+    // Declare function
         //     Take random number from 0 to 1
         //     If number less than 1/3 it is rock
         //     If number less than 2/3 it is paper 
@@ -20,8 +20,51 @@ function getCompuerChoice() {
       }
     
 }
-console.log(getCompuerChoice())
+
 // A function that takes and returns human choice
+
+    //Declare function
+        //input 
+        //Check IF input is valid
+            //While not valid
+                //Input again
+        //If input is R - Rock
+        //If input is P - Paper
+        //If input is S - Scissors
+
+function getHumanChoice() {
+    let humanChoice = prompt("Your turn. Type 'R' for Rock, 'P' for Paper, 'S' for Scissors: ")
+    humanChoice = humanChoice.toUpperCase()
+    
+    if (humanChoice != "R" && humanChoice != "P" && humanChoice != "S") {
+        let valid = 0
+        
+        while (!valid) {
+            humanChoice = prompt("Incorrect value. Type 'R' for Rock, 'P' for Paper, 'S' for Scissors: ")
+            
+            if (humanChoice) {
+                humanChoice = humanChoice.toUpperCase()
+           }
+
+            if (humanChoice == "R"|| humanChoice == "P"|| humanChoice == "S") {
+                valid = 1
+            } else if (humanChoice == null) {
+                alert("You canceled input!")
+                break;
+            }
+        }
+    }
+    
+    switch (humanChoice) {
+        case "R":
+            return "Rock";
+        case "P":
+            return "Paper";
+        case "S":
+            return "Scissors";
+    }
+
+}
 
 // Declare variables that store human and computer score
 
