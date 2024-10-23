627bc9360ff9ba6c5dc5ebc7a09ddf6c00276ee5
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-10-23 15:06:13 -0400

    Add user interface. Refactor code
    Removed old logic with new approach, add understandable intuitive UI

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 console.log("Working")
 
-// A function that returns ramdomly rock paper or scissors
-    
-    // Declare function
-        //     Take random number from 0 to 1
-        //     If number less than 1/3 it is rock
-        //     If number less than 2/3 it is paper 
-        //     If number is higher it is scissors
+const resultContainer = document.querySelector("#resultContainer")
+const btns = document.querySelectorAll("button");
+
+const tryAgain = document.createElement("h1")
+tryAgain.textContent = "Try Again"
+
+
+btns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+       playRound(getComputerChoice(), e.target.id)
+    });
+})
 
 function getComputerChoice() {
     let computerChoice = Math.random()
@@ -21,117 +26,56 @@ function getComputerChoice() {
     
 }
 
-// A function that takes and returns human choice
 
-    //Declare function
-        //input 
-        //Check IF input is valid
-            //While not valid
-                //Input again
-        //If input is R - Rock
-        //If input is P - Paper
-        //If input is S - Scissors
-
-function getHumanChoice() {
-    let humanChoice = prompt("Your turn. Type 'R' for Rock, 'P' for Paper, 'S' for Scissors: ")
-    humanChoice = humanChoice.toUpperCase()
-    
-    if (humanChoice != "R" && humanChoice != "P" && humanChoice != "S") {
-        let valid = 0
-        
-        while (!valid) {
-            humanChoice = prompt("Incorrect value. Type 'R' for Rock, 'P' for Paper, 'S' for Scissors: ")
-            
-            if (humanChoice) {
-                humanChoice = humanChoice.toUpperCase()
-           }
-
-            if (humanChoice == "R"|| humanChoice == "P"|| humanChoice == "S") {
-                valid = 1
-            } else if (humanChoice == null) {
-                alert("You canceled input!")
-                break;
-            }
-        }
-    }
-    
-    switch (humanChoice) {
-        case "R":
-            return "Rock";
-        case "P":
-            return "Paper";
-        case "S":
-            return "Scissors";
-    }
-
-}
-
-// Declare variables that store human and computer score
 let computerScore = 0
 let humanScore = 0
 
-// Round by round take human and computer choices and calculate winner
 
-    //Declare function with choices as parameters
-        //IF player and computer choice are equal - spare
-            //WHILE spare
-                //Try again
-        //IF player choice is Rock and computer choice is Scissors - You win
-        //IF player choice is Paper and computer choice is Rock - You win
-        //IF player choice is Scissors and computer choice is Paper - You win
-        //ELSE - you lose
 
 function playRound(computerValue, humanValue) {
     
-    if (computerValue == humanValue) {
-        let spare = 1
-        while (spare) {
-            console.log("Try again!")
-            computerValue = getComputerChoice()
-            humanValue = getHumanChoice()
-            if (computerValue != humanValue)
-                spare = 0
-
-        }
+    if (document.body.contains(tryAgain)) {
+        document.body.removeChild(tryAgain);
     }
+
+    if (computerValue === humanValue) {
+        document.body.appendChild(tryAgain)
+        return
+        }
+    
     
     if (
         ((humanValue == "Rock") && (computerValue == "Scissors")) ||
         ((humanValue == "Paper") && (computerValue == "Rock")) ||
         ((humanValue == "Scissors") && (computerValue == "Paper"))
     ) {
+            console.log(`Your Choice: ${humanValue}`)
+            console.log(`Computer Choice: ${computerValue}`)
             console.log("You Win!")
-            return humanScore += 1
+            humanScore += 1
     } else {
+        console.log(`Your Choice: ${humanValue}`)
+        console.log(`Your Choice: ${computerValue}`)
         console.log("You Lose!")
-        return computerScore += 1
+        computerScore += 1
     }
-
-}
-
-
-// Logic to play 5 rounds
-
-    //Declare a function
-        //FOR 5 iterations do the following
-            //Call playRound function to play single round
-        //IF humanScore is bigger than computerScore
-            //You win
-        //else - you lose
     
-function playGame () {
-
-    for (let i = 0; i < 5; i++) {
-        playRound(getComputerChoice(), getHumanChoice())
-        console.log(`You: ${humanScore}`)
-        console.log(`Computer: ${computerScore}`)
-    }
-    if (humanScore > computerScore) {
-        console.log("You won the game!")
+    if (humanScore < 5 && computerScore < 5) {
+        resultContainer.innerHTML = `Human: ${humanScore} ${humanValue}<br> 
+                                Computer: ${computerScore} ${computerValue}`
     } else {
-        console.log("You lose the game!")
+        let winner
+        if (humanScore > computerScore) {
+            winner = "Human"
+        } else {
+            winner = "Compuer"
+        }
+        resultContainer.innerHTML = `Winner is ${winner}`
+        computerScore = 0
+        humanScore = 0
     }
-
+    
+    
 }
 
-playGame()
+
